30698ddf4a30bd02e8841209a843a2fb056403ae
Alexandre Croteau <alexandre.croteau@hotmail.ca>
2018-04-09 20:46:54 -0400

    correction du deserializer pour interpreter le resultat de p2 correctement (array of string)

--- ResponseDeserializer.java
+++ ResponseDeserializer.java
@@ -1,11 +1,15 @@
 // https://stackoverflow.com/questions/42348140/jackson-choose-children-class-for-deserialization-depending-of-field-existence
 
+// parti de cet exemple modifié pour nos besoins pour deserialiser les resultats à passer à P4
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResponseDeserializer extends StdDeserializer<Response> {
     public ResponseDeserializer() {
@@ -21,12 +25,15 @@ public class ResponseDeserializer extends StdDeserializer<Response> {
             result = new TranslationResponse();
             ((TranslationResponse)result).setCode(code.asInt());
             ((TranslationResponse)result).setLang(node.findValue("lang").asText());
-            ((TranslationResponse)result).setText(new String[] {"tata"});
+            
+            List<String> texte = new ArrayList<String>();
             
             for(final JsonNode objNode : node.findValue("text"))
             {
-            	
+            	texte.add(objNode.asText());
             }
+            
+            ((TranslationResponse)result).setText(texte.toArray(new String[0]));
             
         } else {
             result = new ResizeResponse();
